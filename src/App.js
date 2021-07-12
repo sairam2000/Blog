f8d623c077ec22cac7cf787ef6ac813170fa3029
sairamnimmakayala8@gmail.com <sairamnimmakayala8@gmail.com>
2021-07-12 18:39:16 +0530

    add postview page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './app.css'
 import Home from "./screens/home/Home";
 import Navbar from './components/navbar/Navbar'
+import Single from './screens/single/Single';
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Home />
+      <Single />
     </div>
   );
 }
---
+++ src/components/singlepost/SinglePost.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import './singlepost.css'
+function SinglePost() {
+    return (
+        <div className="singlepost">
+            <img src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" 
+            alt="singlepost" 
+            className="singlepostImage" 
+            />
+            <h1 className="singlepostTitle">
+            Lorem ipsum dolor
+            <div className="singlepostEdit">
+                <i className="singlepostIcon far fa-edit"></i>
+                <i className="singlepostIcon far fa-trash-alt"></i>
+            </div>
+            </h1>
+            <div className="singlepostInfo">
+                <span>
+                    Author:
+                    <b className="singlepostAuthor">Sairam</b>
+                </span>
+                <span>1 day ago</span>
+            </div>
+            <p className="singlepostDesc">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                Facere quam nulla neque magnam, earum ipsam nihil laborum porro, 
+                repellendus maiores quidem tenetur cumque impedit est, dolore 
+                delectus facilis illo quisquam.Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                Facere quam nulla neque magnam, earum ipsam nihil laborum porro, 
+                repellendus maiores quidem tenetur cumque impedit est, dolore 
+                delectus facilis illo quisquam.Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                Facere quam nulla neque magnam, earum ipsam nihil laborum porro, 
+                repellendus maiores quidem tenetur cumque impedit est, dolore 
+                delectus facilis illo quisquam.Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                Facere quam nulla neque magnam, earum ipsam nihil laborum porro, 
+                repellendus maiores quidem tenetur cumque impedit est, dolore 
+                delectus facilis illo quisquam.Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                Facere quam nulla neque magnam, earum ipsam nihil laborum porro, 
+                repellendus maiores quidem tenetur cumque impedit est, dolore 
+                delectus facilis illo quisquam.Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                Facere quam nulla neque magnam, earum ipsam nihil laborum porro, 
+                repellendus maiores quidem tenetur cumque impedit est, dolore 
+                delectus facilis illo quisquam.
+                <br/>
+                <br/>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                Facere quam nulla neque magnam, earum ipsam nihil laborum porro, 
+                repellendus maiores quidem tenetur cumque impedit est, dolore 
+                delectus facilis illo quisquam.Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                Facere quam nulla neque magnam, earum ipsam nihil laborum porro, 
+                repellendus maiores quidem tenetur cumque impedit est, dolore 
+                delectus facilis illo quisquam.Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                Facere quam nulla neque magnam, earum ipsam nihil laborum porro, 
+                repellendus maiores quidem tenetur cumque impedit est, dolore 
+                delectus facilis illo quisquam.Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                Facere quam nulla neque magnam, earum ipsam nihil laborum porro, 
+                repellendus maiores quidem tenetur cumque impedit est, dolore 
+                delectus facilis illo quisquam.
+            </p>
+
+        </div>
+    )
+}
+
+export default SinglePost
